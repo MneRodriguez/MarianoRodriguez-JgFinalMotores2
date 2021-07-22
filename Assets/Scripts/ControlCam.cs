04efaefd7be09e8ad87813508137ede894d09475
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-22 15:07:48 -0300

    Comienzo de tp final, estableciendo escenas

---
+++ Assets/Scripts/ControlCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCam : MonoBehaviour
+{
+    public GameObject Jugador;    
+    public Vector3 offset;
+    public float movtoCam = 0.5f;
+
+    void Start()
+    {
+        offset = transform.position - Jugador.transform.position;
+                
+    }
+
+    
+    void Update()
+    {
+        Vector3 posDeCam = new Vector3(Jugador.transform.position.x, Jugador.transform.position.y, transform.position.z);
+                
+        // Suavizado del movimiento de la cámara
+        transform.position = Vector3.SmoothDamp(transform.position, posDeCam, ref offset, movtoCam);
+                
+    }
+}
