f8bd1aece63e99eed8f0ad63f6340a265f2dd5ad
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-25 20:13:18 -0300

    Mecanica abrir puerta nivel 2

--- Assets/Scripts/AbrePrtaNvl2.cs
+++ Assets/Scripts/AbrePrtaNvl2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AbrePrtaNvl2 : MonoBehaviour
 {
-    //public Animator AnimPrtaNvl2;
+    public Animator AnimPrtaNvl2;
     public Light LuzNvl2_4;
 
     void Start()
@@ -12,7 +12,7 @@ public class AbrePrtaNvl2 : MonoBehaviour
         GameObject LuzCambiaColor = GameObject.FindWithTag("LuzRojoVerdePrta"); 
         LuzNvl2_4 = LuzCambiaColor.GetComponent<Light>();
 
-        //AnimPrtaNvl2.enabled = false;
+        AnimPrtaNvl2.enabled = false;
     }
 
     
@@ -25,7 +25,7 @@ public class AbrePrtaNvl2 : MonoBehaviour
     {
         if (other.gameObject.CompareTag("CuboLlavePrtaNvl2")) // SI A LA ZONA QUE TRIGGEREA LA ANIM LA TOCA EL "CuboLlave"
         {
-            //AnimPrtaNvl2.enabled = true;
+            AnimPrtaNvl2.enabled = true;
             LuzNvl2_4.color = Color.green;
         }
     }
