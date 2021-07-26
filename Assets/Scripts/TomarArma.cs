e92a4130cc7c84d8004c2dd8eae5390e8926c262
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-26 14:19:46 -0300

    Agarrar arma LOGRADO

---
+++ Assets/Scripts/TomarArma.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TomarArma : MonoBehaviour
+{
+    public GameObject ZonaAgarreDeArmaEnPlayer;
+    bool puedeAgarrarArma;
+
+    GameObject ArmaQueSeRecoge;
+    //bool tieneElArma;
+
+    void Start()
+    {
+        puedeAgarrarArma = false;
+        //tieneElArma = false;
+    }
+
+    
+    void Update()
+    {
+        if (puedeAgarrarArma == true)
+        {
+            ArmaQueSeRecoge.GetComponent<Rigidbody>().isKinematic = true;
+            ArmaQueSeRecoge.transform.position = ZonaAgarreDeArmaEnPlayer.transform.position; // ESTE SERIA EL PRIMER EMPTY CHILD DEL PLAYER
+            ArmaQueSeRecoge.transform.parent = ZonaAgarreDeArmaEnPlayer.transform; // CONVIERTE A LA PISTOLA EN CHILD DE LA ZONA DE AGARRE DEL PLAYER
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pistola")) // SI EL PLAYER TOCA LA PISTOLA...
+        {
+            puedeAgarrarArma = true;
+            ArmaQueSeRecoge = other.gameObject;
+        }
+    }
+}
