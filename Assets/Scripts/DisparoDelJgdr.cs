d4289bf49e6915ed2691660016e8f4ac4219a9a5
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-26 14:08:19 -0300

    Trabajando mecanica pickup pistola y disparo

---
+++ Assets/Scripts/DisparoDelJgdr.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoDelJgdr : MonoBehaviour
+{
+    public Transform ZonaSpawnDelDisparoJgdr;
+    public GameObject balaPrefab;
+
+    public float VelDesplzBalaJgdr = 20f;
+    public float duracionBala = 2.5f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Disparar();
+        }
+    }
+
+    void Disparar()
+    {
+        GameObject balaJgdr = Instantiate(balaPrefab, ZonaSpawnDelDisparoJgdr.position, ZonaSpawnDelDisparoJgdr.rotation);
+        Rigidbody rbala = balaJgdr.GetComponent<Rigidbody>();
+        rbala.AddForce(ZonaSpawnDelDisparoJgdr.transform.forward * VelDesplzBalaJgdr, ForceMode.Impulse);
+
+        Destroy(balaJgdr, duracionBala);
+    }
+}
