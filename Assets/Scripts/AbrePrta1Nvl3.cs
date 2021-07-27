a981b91835b92fc33164aa9b2a444b98f8c17ee7
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-27 17:55:59 -0300

    Abrir puerta 1 de nivel 3 hecho

---
+++ Assets/Scripts/AbrePrta1Nvl3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrePrta1Nvl3 : MonoBehaviour
+{
+    public Animator AbrirPrta1Nvl3;
+    void Start()
+    {
+        AbrirPrta1Nvl3.enabled = false;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            AbrirPrta1Nvl3.enabled = true;
+        }
+    }
+}
