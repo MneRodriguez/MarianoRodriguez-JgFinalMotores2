41980b4c56ecb0279f0ce3294a9d5425b0b1bc3b
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-27 17:33:19 -0300

    Trigger de plataforma nivel3

---
+++ Assets/Scripts/BajaPltfmNvl3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BajaPltfmNvl3 : MonoBehaviour
+{
+    public Animator AnimBajarPltfmNvl3;
+    void Start()
+    {
+        AnimBajarPltfmNvl3.enabled = false;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("CuboLlavePltfmNvl3"))
+        {
+            AnimBajarPltfmNvl3.enabled = true;
+        }
+    }
+}
