25dfcf13fb2c7c8262a8fb2b90cc38f6bff3e2d5
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-07-27 23:32:06 -0300

    Balajgdr elimina enems

--- Assets/Scripts/IAenemigos.cs
+++ Assets/Scripts/IAenemigos.cs
@@ -18,7 +18,7 @@ public class IAenemigos : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player")) // SI LOS ENEMS SON TOCADOS POR EL PLAYER, SE DESTRUYEN Y LE SUMAN PUNTOS
+        if (collision.gameObject.CompareTag("BalaJgdr")) // SI LOS ENEMS SON TOCADOS POR LAS BALAS DEL PLAYER, SE DESTRUYEN Y LE SUMAN PUNTOS
         {
             //ContadorDeScore.score += 2; FALTA HACERLO
             Destroy(gameObject);
